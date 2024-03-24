902bdbf366bd976f94ee05556aa692ad459dac7b
yasincelebi <yasin.celebiks@reviewsolicitors.com>
2024-03-24 17:37:04 +0000

    create personal website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+import {Github, Linkedin} from "lucide-react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex min-h-screen flex-col p-4 flex-1 container items-center gap-4 max-w-2xl mx-auto">
+            <h1 className="text-3xl font-bold">
+                Yasin Çelebi
+            </h1>
+            <p className="text-lg text-center">
+                Frontend Developer
+            </p>
+            <div className="flex flex-col space-y-4 text-xs">
+                <p className='text-base'>
+                    Hi, I&apos;m Yasin,  software developer with experience in developing front-end and back-end systems.
+                </p>
+                <p>
+                    Outstanding individual contributor and a fantastic team player; illustrates the true meaning of
+                    teamwork, and creates good, long-lasting relationships with others along the way.
+                </p>
+                <p>
+                    Strategic thinker/nonconformist; striving to make the best of each and every situation.
+                </p>
+                <p>
+                    Self-starter/Internally motivated who draws energy from unknowns.
+                </p>
+                <p>
+                    Capable of working very independently with excellent quality and having a natural desire to develop
+                    new skills constantly.
+                </p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <section id='experience' className="flex flex-col space-y-4 w-full">
+                <h2 className="text-2xl font-bold">Experience</h2>
+                <div className="flex flex-col space-y-4">
+                    <div>
+                        <h3 className="text-lg font-medium">Frontend Developer</h3>
+                        <div className="text-sm my-2">
+                            Developing ReviewSolicitors, a platform offering client feedback for UK law firms.
+                        </div>
+                        <p className="text-sm">2023 - Present</p>
+                        <p className="text-sm">ReviewSolicitors</p>
+                        <p className="text-sm">Nottingham, UK</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                        <div className="flex space-x-2 text-xs mt-1">
+                            <span>React</span>
+                            <span>Java/Spring Boot</span>
+                            <span>Typescript</span>
+                            <span>PostgreSQL</span>
+                        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                    </div>
+                    <div>
+                        <h3 className="text-lg font-medium">Senior Frontend Developer</h3>
+                        <p className="text-sm my-2">
+                            Worked as a senior frontend developer in a scrum team specializing in developing fast
+                            payment methods in restaurants via QR code as well as order-tracking systems. Led the
+                            payments team.
+                        </p>
+                        <p className="text-sm">2022 - 2023</p>
+                        <p className="text-sm">Qlub</p>
+                        <p className="text-sm">Istanbul, Turkey</p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                        <div>
+                            <div className="flex space-x-2 text-xs mt-1">
+                                <span>React</span>
+                                <span>Node.js</span>
+                                <span>Typescript</span>
+                                <span>Web Components</span>
+                                <span>CLI Apps/Bash</span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <h3 className="text-lg font-medium">Frontend Developer</h3>
+                        <p className="text-sm my-2">
+                            Frontend development of education platform that used by thirty million unique users.
+                        </p>
+                        <p className="text-sm">2021 - 2022</p>
+                        <p className="text-sm">Sebit</p>
+                        <p className="text-sm">Ankara, Turkey</p>
+                        <div>
+                            <div className="flex space-x-2 text-xs mt-1">
+                                <span>React</span>
+                                <span>Angular/AngularJS</span>
+                                <span>Typescript</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="flex space-x-4">
+                <a target="_blank" href="https://www.github.com/yasincelebi" className="text-lg font-medium" rel="noopener noreferrer">
+                    <Github size={24}/>
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/celebiyasinn" className="text-lg font-medium" rel="noopener noreferrer">
+                    <Linkedin size={24}/>
+                </a>
+            </div>
+        </main>
+    );
 }
