25f7f1240769f589b82fd16681ca59f8aa38f748
yasincelebi <yasin.celebiks@reviewsolicitors.com>
2024-04-04 09:44:08 +0100

    add gtm with next third parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {clsx} from "clsx";
-import Head from 'next/head'
-
+import { clsx } from "clsx";
+import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Yasin Çelebi - Frontend Developer",
-  description: "Yasin Çelebi's portfolio website showcasing his experience as a frontend developer.",
+  description:
+    "Yasin Çelebi's portfolio website showcasing his experience as a frontend developer.",
   applicationName: "Yasin Çelebi",
   authors: {
     name: "Yasin Çelebi",
     url: "https://yasincelebi.com",
   },
   category: "Portfolio",
-    keywords: ["Yasin Çelebi", "Frontend Developer", "React", "Next.js","TypeScript"],
+  keywords: [
+    "Yasin Çelebi",
+    "Frontend Developer",
+    "React",
+    "Next.js",
+    "TypeScript",
+  ],
   robots: "index, follow",
 };
-
 
 export default function RootLayout({
   children,
@@ -27,22 +33,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WNZFZWRC');</script>
-      
-      </Head>
-      <body className={clsx(" text-black dark:text-white bg-white dark:bg-black", inter.className)}>
+    <html lang="en">
+      <GoogleTagManager gtmId="GTM-WNZFZWRC" />
 
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNZFZWRC"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-      {children}
+      <body
+        className={clsx(
+          " text-black dark:text-white bg-white dark:bg-black",
+          inter.className
+        )}
+      >
+        {children}
       </body>
-    </>
+    </html>
   );
 }
