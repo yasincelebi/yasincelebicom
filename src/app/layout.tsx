8fafd8381e4ec17c05394d113873dded7e2b0013
Yasin Celebi <71718079+yasincelebi@users.noreply.github.com>
2024-04-04 09:32:34 +0100

    add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {clsx} from "clsx";
+import Head from 'next/head'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +28,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WNZFZWRC');</script>
+      
+      </Head>
       <body className={clsx(" text-black dark:text-white bg-white dark:bg-black", inter.className)}>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNZFZWRC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
       {children}
       </body>
     </html>
